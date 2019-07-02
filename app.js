c36f19d792eba13e5970beeaa11152288964796b
Swarts, Matthew E <strontium5@gmail.com>
2019-07-02 08:19:10 -0400

    initial commit with basic webserver

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+
+app.use(express.static(__dirname + '/public')); 
+
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap JS, CSS, and fonts
+
+var port = process.env.PORT || 80;
+server.listen(port);
