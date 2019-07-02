8cb14128cc2f0e2a40e9a644e831f7629141e75c
Swarts, Matthew E <strontium5@gmail.com>
2019-07-02 09:00:50 -0400

    added basic tcp client with net

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const net = require('net');
 const io = require('socket.io')(server);
 
 
@@ -14,3 +15,19 @@ app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // red
 
 var port = process.env.PORT || 80;
 server.listen(port);
+
+
+var tcpclient = new net.Socket();
+tcpclient.connect(1337, '127.0.0.1', function() {
+	console.log('Connected');
+	tcpclient.write('Hello, server! Love, Client.');
+});
+
+tcpclient.on('data', function(data) {
+	console.log('Received: ' + data);
+	tcpclient.destroy(); // kill client after server's response
+});
+
+tcpclient.on('close', function() {
+	console.log('tcp connection closed');
+});
